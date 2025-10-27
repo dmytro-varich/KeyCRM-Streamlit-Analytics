fcc8b8249614e36394716ef017d6ad1b5ff33260
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2025-10-27 13:09:47 +0000

    Refactor time_utils to use SERVER_TZ for UTC conversion in get_kyiv_date function; add empty __init__.py files in config, src, api, components, and utils directories

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -8,7 +8,7 @@ today_date = datetime.strptime(today, "%Y-%m-%d").date()
 
 def get_kyiv_date(created_at_str: str) -> str:
     try:
-        dt_utc = datetime.strptime(created_at_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+        dt_utc = datetime.strptime(created_at_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=SERVER_TZ)
         dt_kyiv = dt_utc.astimezone(KYIV_TZ)
         return dt_kyiv.strftime("%Y-%m-%d")
     except Exception:
